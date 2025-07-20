b1366d5f20abd1390c141be4213ba0b47fafbc5a
geekmedico <greer@geekmedico.com>
2025-07-19 18:48:59 -0600

    Update ToListAsync to avoid IDbAsyncEnumerable requirement

--- FileContextCore/Extensions/QueryableExtensions.cs
+++ FileContextCore/Extensions/QueryableExtensions.cs
@@ -3,26 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 
 namespace FileContextCore.Extensions
 {
     public static class QueryableExtensions
     {
-        public static async Task<List<T>> ToListAsync<T>(this IQueryable<T> source, CancellationToken cancellationToken = default)
+        public static Task<List<T>> ToListAsync<T>(this IQueryable<T> source, CancellationToken cancellationToken = default)
         {
             if (source == null)
             {
                 throw new ArgumentNullException(nameof(source));
             }
 
-            var list = new List<T>();
-            await foreach (var element in source.AsAsyncEnumerable().WithCancellation(cancellationToken))
-            {
-                list.Add(element);
-            }
-
-            return list;
+            return Task.Run(() => source.ToList(), cancellationToken);
         }
     }
 }
